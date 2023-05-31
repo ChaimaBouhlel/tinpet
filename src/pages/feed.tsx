8ea41225355ad693fa9ba82bd246772403b6f7c7
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2023-05-31 22:47:50 +0100

    add feed page and its components structure

---
+++ src/pages/feed.tsx
@@ -0,0 +1,23 @@
+
+import {NextPageWithLayout} from "@/types/global";
+import {ReactElement} from "react";
+import Layout from "@/layout";
+
+const Feed: NextPageWithLayout<{}> = () => {
+    return (
+        <>
+            <div>
+                Hello hethia page l feed ay haja theb tarmi tarmiha houuyniiiiiiii!!!!!
+            </div>
+        </>
+    )
+}
+
+Feed.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    );
+};
+export default Feed;
