6354630f63f4b7700185d4a97dd3f1a10621c0d6
nada <nadakhelif61@gmail.com>
2023-06-01 02:31:44 +0100

    form create annone

---
+++ src/pages/formPage2.jsx
@@ -0,0 +1,12 @@
+import Form2 from '../components/forms/form2';
+
+const formPage2 = () => {
+  return (
+    <div>
+      <h1>Form Page</h1>
+      <Form2 />
+    </div>
+  );
+};
+
+export default formPage2;
